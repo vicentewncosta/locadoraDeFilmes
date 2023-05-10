bfd7d8bf872462ae773865bb2ba76a88d9373973
Vicente Costa <89530296+vicentewncosta@users.noreply.github.com>
2023-05-10 13:20:47 -0300

    Update FilmeController.cs

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,12 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
 
-namespace locadora.Controllers
-{
-    public class FilmeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
